8c52cd8c835e1e6cdf49b9757647f9074561d6e0
Alen Tito <61512914+Alentito@users.noreply.github.com>
2025-07-08 20:25:10 +0530

    Update brokerApi.js

--- frontend/src/api/brokerApi.js
+++ frontend/src/api/brokerApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const brokerApi = createApi({
   reducerPath: 'brokerApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://backend-l3f9.onrender.com/api/' }),
   tagTypes: ['Broker'],
   endpoints: (build) => ({
     getBrokers: build.query({
