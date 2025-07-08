b3bf20b4f861c55ec000804b12686672a729c821
Alen Tito <61512914+Alentito@users.noreply.github.com>
2025-07-08 20:24:42 +0530

    Update loanApi.js

--- frontend/src/api/loanApi.js
+++ frontend/src/api/loanApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const loanApi = createApi({
   reducerPath: "loanApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://backend-l3f9.onrender.com/api/" }),
   tagTypes: ["Loan"], // For caching and invalidation
   endpoints: (builder) => ({
     getLoans: builder.query({
