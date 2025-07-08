3f3b6f0552192f49c079c6c7926531f6f10a7074
Alen Tito <61512914+Alentito@users.noreply.github.com>
2025-07-08 20:18:50 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # 👈 Your global static folder
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",  # 👈 Your global static folder
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
