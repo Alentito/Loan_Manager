27699a4aceca71c24110847513714e939c3ad06f
KISHOR KV <120302979+Kishor284@users.noreply.github.com>
2025-10-10 17:19:15 +0530

    Update EmployeeList.jsx

--- frontend/src/employees/EmployeeList.jsx
+++ frontend/src/employees/EmployeeList.jsx
@@ -154,7 +154,7 @@ const EmployeeList = () => {
   };
 
   if (format && map[format]) {
-    window.open(`http://localhost:8000/api/${map[format]}`, '_blank');
+    window.open(`https://backend-l3f9.onrender.com/api/${map[format]}`, '_blank');
   }
 };
 
