3b23b5355c7ba41e0b0bd5c5f5879bbb1692329d
Alen Tito <61512914+Alentito@users.noreply.github.com>
2025-10-12 23:12:37 +0100

    Remove Lender model from admin registration
    Removed Lender model registration from admin.

--- backend/loan/admin.py
+++ backend/loan/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Loan,  Lender, Task, Notification,EventOutbox, Milestone,IncomeAssetNote
+from .models import Loan,   Task, Notification,EventOutbox, Milestone, IncomeAssetNote
 
 admin.site.register(Loan)
 
-admin.site.register(Lender)
+
 
 admin.site.register(Task)
 admin.site.register(Notification)
 admin.site.register(EventOutbox)
 admin.site.register(Milestone)
 admin.site.register(IncomeAssetNote)
+
