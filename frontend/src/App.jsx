dd97897a737cb3aa5a2fd7c74e95e8a18a59dc84
Alen Tito <61512914+Alentito@users.noreply.github.com>
2025-10-12 18:30:47 +0100

    Add useLocation import from react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
