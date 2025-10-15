d56dfb11de9d91edb0ab9596880085e1856c5b40
Alen Tito <61512914+Alentito@users.noreply.github.com>
2025-10-15 16:42:41 +0100

    Change tooltip to display username instead of fullname

--- frontend/src/layout/Topbar.jsx
+++ frontend/src/layout/Topbar.jsx
@@ -203,7 +203,7 @@ export default function TopNavbar({ mode, setMode, onOpenSidebar = () => {} }) {
         </Menu>
 
         {/* Avatar (lucide User used as fallback when avatarSrc is missing) */}
-        <Tooltip title={`${user.Firstname} ${user.lastName}`}>
+        <Tooltip title={`${user.username}`}>
           <IconButton
             onClick={handleUserClick}
             size="small"
@@ -240,7 +240,7 @@ export default function TopNavbar({ mode, setMode, onOpenSidebar = () => {} }) {
           }}
         >
           <Box px={2} py={1} fontWeight="bold">
-            {user.firstName} {user.lastName}
+            {user.username} {user.lastName}
           </Box>
           <Divider />
           <MenuItem
