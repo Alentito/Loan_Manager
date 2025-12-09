ab3ea375d11022c63e4d5d066ce9bdfd983410b1
Alen Tito <61512914+Alentito@users.noreply.github.com>
2025-12-09 19:38:42 +0000

    Update models.py

--- backend/userauth/models.py
+++ backend/userauth/models.py
@@ -22,10 +22,23 @@ class Role(models.Model):
     description = models.TextField(blank=True)
     permissions = models.ManyToManyField(Permission, blank=True)
     # optional: role hierarchy
-    parents = models.ManyToManyField('self', symmetrical=False, blank=True) 
+    parents = models.ManyToManyField('self', symmetrical=False, blank=True)
+    sort_order = models.PositiveIntegerField(default=0)
+
+
 
     def __str__(self):
         return self.name
+    class Meta:
+        ordering = ["sort_order", "name"]
+
+
+from django.contrib.auth.models import Group
+class RoleMetadata(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE, related_name="metadata")
+    sort_order = models.PositiveIntegerField(default=0)
+    assignable_on_loan = models.BooleanField(default=False)  # <--- Add this field
+    
 
 # class Employee(models.Model):
 #     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
