1e087a3bacb87c24fc8d0407265623f5d52a37b2
Alen Tito <61512914+Alentito@users.noreply.github.com>
2026-01-13 20:04:51 +0000

    Remove LoanMilestoneHistory creation from serializers
    Removed milestone history creation logic from loan update and create methods.

--- backend/loan/serializers.py
+++ backend/loan/serializers.py
@@ -11,7 +11,7 @@ from employee.serializers import BrokerSerializer, LoanOfficerSerializer, Employ
 from employee.models import Broker, LoanOfficer, Employee,Lender
 
 
-from .models import Notification, LoanMilestoneHistory
+from .models import Notification
 
 from rest_framework import serializers
 from .models import Milestone,LoanRoleAssignment
@@ -379,17 +379,10 @@ class LoanSerializer(serializers.ModelSerializer):
             employees = item.pop('employees', [])
             ra = LoanRoleAssignment.objects.create(loan=loan, **item)
             ra.employees.set(employees)
-
-        if loan.milestone:
-            LoanMilestoneHistory.objects.create(
-                loan=loan,
-                milestone=loan.milestone
-            )
         return loan
 
     def update(self, instance, validated_data):
         ras = validated_data.pop('role_assignments', None)
-        old_milestone = instance.milestone
         loan = super().update(instance, validated_data)
         if ras is not None:
             LoanRoleAssignment.objects.filter(loan=loan).delete()
@@ -397,17 +390,4 @@ class LoanSerializer(serializers.ModelSerializer):
                 employees = item.pop('employees', [])
                 ra = LoanRoleAssignment.objects.create(loan=loan, **item)
                 ra.employees.set(employees)
-
-            if (
-                old_milestone != loan.milestone
-                and loan.milestone
-                and not LoanMilestoneHistory.objects.filter(
-                    loan=loan,
-                    milestone=loan.milestone
-                ).exists()
-            ):
-                LoanMilestoneHistory.objects.create(
-                    loan=loan,
-                    milestone=loan.milestone
-                )
         return loan
