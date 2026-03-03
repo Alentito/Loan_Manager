0bc45bb9bc3e242bfdce1730f76682313ce9d60a
KISHOR KV <120302979+Kishor284@users.noreply.github.com>
2026-03-03 15:13:17 +0530

    Add task to process login attendance

--- backend/employee/tasks.py
+++ backend/employee/tasks.py
@@ -9,3 +9,16 @@ def mark_daily_absents(self):
     for emp in employees:
         mark_missing_absents(emp)
     return "Daily absent marking completed"
+
+@shared_task
+def process_login_attendance(user_id):
+    from django.contrib.auth import get_user_model
+    from django.utils import timezone
+    from employee.utils import mark_attendance_on_login
+
+    User = get_user_model()
+    try:
+        user = User.objects.get(id=user_id)
+        mark_attendance_on_login(user, login_dt=timezone.now())
+    except Exception:
+        pass
